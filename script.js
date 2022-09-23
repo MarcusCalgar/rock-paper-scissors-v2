e07a729cebb45c8b26ed92c95abea194d6497e9a
MarcusCalgar <paraszthybalazs@gmail.com>
2022-09-23 10:13:48 +0200

    Complete functionality for a single round

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
-    // greet the user
+//MAIN
 
+//Initialize score
+let numberOfRound = 1;
+let playerScore = 0;
+let computerScore = 0;
+
+// Greet the user
 console.log("Welcome to Rock, Paper, Scissors!");
+console.log(`Round ${numberOfRound}`);
+// Get the user's choice
+let playerSelection = getPlayerSelection();
+//Calculate the computer's choice
+let computerSelection = getComputerSelection();
+//Print out the choices
+console.log(`You picked: ${playerSelection}.`);
+console.log(`The computer's choice is: ${computerSelection}`);
+// Evaluate round
+let result = playRound(playerSelection, computerSelection);
+//Print the results to the console
+printResults(result);
+    
+//FUNCTIONS
 
-    // get the user's input
-
-let playerSelection = prompt("Please type in your choice: 'rock', 'paper', or 'scissors'!").toLowerCase();
-
-    // evaluate the user's choice, and let it play again if it isn't correct
-
-while(playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors"){
-    playerSelection = prompt("Sorry, your choice is invalid. Please type in your choice: 'rock', 'paper', or 'scissors'!").toLowerCase();
+// Get the user's input
+function getPlayerSelection(){
+    let playerChoice = prompt("Please type in your choice: 'rock', 'paper', or 'scissors'!").toLowerCase();
+// Evaluate the user's choice, and let them play again if it isn't correct
+    while(playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors"){
+        playerChoice = prompt("Sorry, your choice is invalid. Please type in your choice: 'rock', 'paper', or 'scissors'!").toLowerCase();
+    }    
+    return playerChoice;
 }
-console.log(`Your selection is: ${playerSelection}.`);
 
-    // get the computer's choice
-
+// Get the computer's choice 
 function getComputerSelection(){
-    const matcherNumber = Math.random();
+    let matcherNumber = Math.random();
     if (matcherNumber < 0.33){
         return "rock";
     } else if (matcherNumber < 0.66){
@@ -24,5 +42,31 @@ function getComputerSelection(){
     } else return "scissors";
 }
 
-    // evaluate
-    // print the results to the console
+function playRound(playerSelection, computerSelection){    
+// Evaluate the round
+    let result = "";
+    numberOfRound++;
+    if(playerSelection === computerSelection){        
+        result = "It's a tie!"
+    } else if (
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "rock")
+        ){
+        computerScore++;
+        result = "You lost this round!"
+    } else {
+        playerScore++;
+        result = "You won this round!";
+    }
+    return result;
+};
+
+function printResults(result){    
+    console.log(`The result of this round is: ${result}`);
+    console.log(`Your score is: ${playerScore}`);
+    console.log(`The computer's score is: ${computerScore}`);
+}
+
+
+    
